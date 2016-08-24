b5a4051e3e881e86257080b932d50ec9fcc53a8a
lesmetre <mulaynb@live.fr>
2016-08-24 15:06:53 +0200

    Create ft_create_elem.c

---
+++ J11/ft_create_elem.c
@@ -0,0 +1,15 @@
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*elem;
+
+	elem = NULL;
+	elem = (t_list*)malloc(sizeof(*elem));
+	if (elem)
+	{
+		elem->data = data;
+		elem->next = NULL;
+	}
+	return (elem);
+}
